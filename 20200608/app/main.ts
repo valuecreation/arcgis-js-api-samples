b088902e739b5afc87deb781a73378cfdddb5a1f
kamiya <only@jp.bigplanet.com>
2020-06-08 00:48:23 +0900

    add arcgis-js-api sample typescript

---
+++ 20200608/app/main.ts
@@ -0,0 +1,36 @@
+import WebMap from "esri/WebMap";
+import MapView from "esri/views/MapView";
+import LayerList from "esri/widgets/LayerList";
+
+import esri = __esri;
+
+const map = new WebMap({
+    portalItem: {
+        id: "d5dda743788a4b0688fe48f43ae7beb9"
+    }
+});
+
+const view = new MapView({
+    container: "viewDiv",
+    map: map
+});
+
+const layerList = new LayerList({
+    view,
+    listItemCreatedFunction: event => {
+        const item: esri.ListItem = event.item;
+        if (item.layer.type != 'group') {
+            item.panel = {
+                content: "legend",
+                open: true,
+            } as esri.ListItemPanel;
+        }
+    }
+})
+
+view.ui.add(layerList, "top-right");
+
+
+
+
+
